e324798ad67065bb2b5a1033531f32eebe4b635b
Nechita-Andrei <62114344+Nechita-Andrei@users.noreply.github.com>
2023-05-28 12:59:11 +0300

    refactoring

--- predictBreed.py
+++ predictBreed.py
@@ -1,34 +1,27 @@
-from flask import Flask, request, jsonify
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score
-from sklearn.preprocessing import OneHotEncoder
+from flask import Flask, request, jsonify
 from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import OneHotEncoder
 
 app = Flask(__name__)
 
-# Load data from CSV file
 data = pd.read_csv("dog_breeds_extended.csv")
 
-# Identify categorical columns
 categorical_cols = ['size', 'allergy', 'trainability', 'energy', 'kids', 'grooming']
 
-# Encode categorical columns using OneHotEncoder
 preprocessor = ColumnTransformer(
     transformers=[('cat', OneHotEncoder(), categorical_cols)],
     remainder='passthrough')
 
-# Fit the preprocessor and transform the data
 X = data.drop("breed", axis=1)
 y = data["breed"]
 X_encoded = preprocessor.fit_transform(X)
 
-# Train a random forest classifier on the encoded data
 rf = RandomForestClassifier()
 rf.fit(X_encoded, y)
 
 
-# Define a function to predict the dog breed based on user input
 @app.route('/predict', methods=['POST'])
 def predict_breed():
     size = request.json['size']
@@ -41,12 +34,6 @@ def predict_breed():
     user_data = [[size, allergy, trainability, energy, kids, grooming]]
     user_data_encoded = preprocessor.transform(pd.DataFrame(user_data, columns=categorical_cols))
     breed = rf.predict(user_data_encoded)
-    accuracy_scores = cross_val_score(rf, X_encoded, y, cv=5)  # Change cv value as desired
-
-    # Calculate the mean accuracy score
-    mean_accuracy = accuracy_scores.mean()
-
-    print("Accuracy:", mean_accuracy)
 
     response = {'breed': breed[0]}
     return jsonify(response)
